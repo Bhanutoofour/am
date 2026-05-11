45e9c62c3ce1ea756bc3c0dbf6305a3542f40a51
santhoshisushma-alt <santhoshisushma@gmail.com>
2026-05-11 10:19:35 +0530

    Improve Zoho widget compatibility on Safari

--- app/layout.tsx
+++ app/layout.tsx
@@ -147,12 +147,11 @@ export default function RootLayout({
               }
             };
             var d = document;
-            s = d.createElement('script');
+            var s = d.createElement('script');
             s.type = 'text/javascript';
             s.id = 'zsiqscript';
-            s.defer = true;
             s.src = 'https://salesiq.zoho.in/widget';
-            t = d.getElementsByTagName('script')[0];
+            var t = d.getElementsByTagName('script')[0];
             t.parentNode.insertBefore(s, t);
           `,
         }}
